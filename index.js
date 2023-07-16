8c825500829bf49f9bc1919a38f32dc52e95a4bf
Myriam JAYLE <myriamjayle@gmail.com>
2023-07-16 16:06:53 +0200

    compteurs moteur

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// **************************** Code pour le Parallalax
 let moon = document.getElementById('lune');
 let sea = document.getElementById('mer');
 let text = document.getElementById('text');
@@ -13,5 +14,32 @@ window.addEventListener('scroll', function(){
     text.style.marginRight = value * 4 + 'px';
     text.style.marginTop = value * 1.2 + 'px';
     btn.style.marginTop = value * 1.2 + 'px';
-    header.style.top = value * 0.5 + 'px';
+    header.style.top = value * 0.85 + 'px';
 })
+
+
+// **************************** Code pour les compteurs
+const rating = document.getElementsByClassName('rating')[0];
+const block = document.getElementsByClassName('block');
+
+for (let i = 0; i < 100; i++) {
+    rating.innerHTML += "<div class='block'></div>";
+    block[i].style.transform = "rotate("+3.6 * i +"deg)"
+    // 360deg / 100 = 3.6deg
+    block[i].style.animationDelay = `${i/40}s`;
+}
+
+const counter = document.querySelector('.counter');
+counter.innerText = 0;
+const target = +counter.getAttribute('data-target');
+
+const NumberCounter = () => {
+    const value = +counter.innerText;
+    if (value < target){
+        counter.innerText = Math.ceil(value + 1);
+        setTimeout(() => {
+            NumberCounter();
+        },20)
+    }
+}
+NumberCounter()
